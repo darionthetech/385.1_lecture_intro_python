5b9168abb5eb03264acd5da67df2a59fe27c390d
darionthetech <darionthetech@gmail.com>
2026-04-22 13:14:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Python Revision
+- to run python script `python <filename>` or `python3 <filename>`
+
+## Quick keys:
+- open and close terminal `ctrl + ~`
+- clearing /cleaning terminal: type `clear` or `ctrl + l` 
+- to view previous terminal commands cycle `up-arrow` or `down arrow`
+- to toggle a comment `ctrl + ?`
+- 3 single or three double quotes for multi-line comments
+
+##Built-in functions:
+- `print()` - prints values to to the console
+- `input()` - opposite of print. gets input from user
---
+++ welcome.py
@@ -0,0 +1,24 @@
+# Single line comments
+print('Welcome to Python!!!') #this line does xyz
+
+print('Per Scholas is Unlocking Potential and Changing the Face of Tech')
+
+print(10 + 90)
+
+print(300.5 + 0.5)
+
+print(12, 24, -2, sep=':')
+
+print('but', 'not', 'including', sep='**')
+
+print('but', 'not', 'including', sep=' ')
+
+name = input('What is your name?')
+print('Hello', name, '\n what is up?')
+
+#get numbers from user
+mb = input('Enter your mobile number: ')
+hp = input('Enter your home number: ')
+
+print('Mobile number is: ', mb)
+print('Home number is: ', hp)
